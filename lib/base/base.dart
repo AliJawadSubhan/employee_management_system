5fa61e5ee76f5ed1b64508bb3f32a9ae75cfe8b5
Hassaan Ahmed <106430586+HassaanAhmed60211@users.noreply.github.com>
2024-01-23 18:09:23 +0500

    Updates Jan 23
    added primary color

--- lib/base/base.dart
+++ lib/base/base.dart
@@ -1,3 +1,4 @@
+import 'package:employee_management_system/modules/employee_stats/employee_stats.dart';
 import 'package:flutter/material.dart';
 
 class EmployeeManagementSystem extends StatelessWidget {
@@ -12,7 +13,7 @@ class EmployeeManagementSystem extends StatelessWidget {
         useMaterial3: true,
         indicatorColor: Colors.white,
       ),
-      // home: const SplashPage(),
+      home: const EmployeeStatsPage(),
     );
   }
 }
---
+++ lib/modules/employee_stats/employee_stats.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class EmployeeStatsPage extends StatelessWidget {
+  const EmployeeStatsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            children: [CircleAvatar()],
+          )
+        ],
+      ),
+    );
+  }
+}
