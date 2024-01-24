4a6a595f1c7bd0107b15765f7dea8761a3376d8c
Hassaan Ahmed <106430586+HassaanAhmed60211@users.noreply.github.com>
2024-01-24 19:06:16 +0500

    Updates Jan 24
    Added new Datepicker on attendence page

--- lib/modules/attendence/attendence_provider.dart
+++ lib/modules/attendence/attendence_provider.dart
@@ -1,19 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AttendenceProvider extends ChangeNotifier {
-  bool isVal = false;
   String selectedStartDate = '21/01/2024';
   String selectedEndDate = '21/01/2024';
   TextEditingController dateController = TextEditingController();
-
-  void isDate() {
-    if (isVal == true) {
-      isVal = false;
-    } else {
-      isVal = true;
-    }
-    notifyListeners();
-  }
 
   void showDate() {
     dateController.text = '$selectedStartDate to $selectedEndDate';
